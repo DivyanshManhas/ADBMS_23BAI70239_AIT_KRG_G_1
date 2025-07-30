5d398c89e556769b1fcbc969ab628c91e372ea01
Dde1ta <divyanshmanhasdm2005@gmail.com>
2025-07-30 09:57:01 +0530

    add EXP2

---
+++ Exp2/Note.sql
@@ -0,0 +1,24 @@
+/**
+	Types of Join
+
+		1. Inner join :- Common data b/w the both tables
+		2. Left Outer Join :- The whole left table's data (Before the the join)
+		3. Right outer Join :- The whole right table's data (After the the join)
+		4. Full outer Join :- Full outer join (L + R + Comman Data)
+		5. Self Join
+		6. Left Exclusive join
+		7. Right Exclusive join
+
+	Syntax of JOINS :-
+
+		1. INNER JOIN :-
+
+			SELECT <columns>
+			FROM <Left Table>
+			INNER JOIN <Right Table>
+			ON <Condition>;
+
+		2. 
+		
+
+*/
